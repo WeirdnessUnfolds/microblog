2f3c71abdbcfa34e636adde9e2bd6fb9afc31b9b
simonking99 <sibo18@student.bth.se>
2025-03-30 21:49:05 +0200

    cd - Created docker publish yml and made build and test yml Made a github actions workflow that depends on the CI build and test workflow completing that pushes the Dockerfile_prod image to dockerhub.

--- app/__init__.py
+++ app/__init__.py
@@ -64,7 +64,7 @@ def create_app(config_class=ProdConfig):
             '[%(asctime)s %(levelname)s] %(remote_addr)s requested %(url)s\n: %(message)s [in %(module)s:%(lineno)d]'
         )
         default_handler.setFormatter(formatter)
-        app.logger.setLevel(logging.INFO)
+        app.logger.setLevel(logging.DEBUG)
 
     return app
 
